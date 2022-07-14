4df5a90c83c48d80ff9e8327581c4eacfedfab49
xueyueli <xueyueli@futunn.com>
2022-07-14 20:38:37 +0800

    .

---
+++ log_util/zap.go
@@ -0,0 +1,63 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+func Debug(msg string, fields ...zap.Field) {
+	logger.Debug(msg, fields...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	logger.Info(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	logger.Error(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	logger.Warn(msg, fields...)
+}
+
+func init() {
+	var err error
+	logger, err = getConfig().Build()
+	if err != nil {
+		panic(err)
+	}
+	logger = logger.WithOptions(zap.AddCallerSkip(1))
+	logger.WithOptions()
+	logger.Info("Logger init success")
+}
+
+func getConfig() zap.Config {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder, //这里可以指定颜色
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.NanosDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	// 设置日志级别
+	atom := zap.NewAtomicLevelAt(zap.DebugLevel)
+	config := zap.Config{
+		Level:            atom,               // 日志级别
+		Development:      true,               // 开发模式，堆栈跟踪
+		Encoding:         "console",          // 输出格式 console 或 json
+		EncoderConfig:    encoderConfig,      // 编码器配置
+		OutputPaths:      []string{"stdout"}, // 输出到指定文件 stdout（标准输出，正常颜色） stderr（错误输出，红色）
+		ErrorOutputPaths: []string{"stderr"},
+	}
+	config.EncoderConfig.EncodeLevel = zapcore.LowercaseColorLevelEncoder //这里可以指定颜色
+	return config
+}
